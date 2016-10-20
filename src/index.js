e414b88863cef30589194b874940c3042cdd4a9e
andy mccoy <andy.k.mccoy@gmail.com>
2016-10-19 21:04:00 -0700

    added basic security stuff
    * helmet
    * nsp

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const helmet = require('helmet');
+const app = require('./app');
+
+app.use(helmet());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
