4c11e471f993d1998869b2b8ce9f4954fc67e83f
andy mccoy <andy.k.mccoy@gmail.com>
2016-10-23 13:53:27 -0700

    basic setup
    * test route
    * nodemon

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
+const helmet = require('helmet');
 
 const app = express();
+
+app.use(helmet());
+
+app.get('/', function (req, res) {
+  res.send('ok');
+})
 
 module.exports = app;
