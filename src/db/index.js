c6a88a88db9d630ad927a7ff0f74ea271ff745ff
andy mccoy <andy.k.mccoy@gmail.com>
2016-11-19 20:02:12 -0800

    setup default env variable

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,10 @@
 const neo4j = require('neo4j-driver').v1;
 const nconf = require('nconf');
+const env = process.env.NODE_ENV || 'development';
 
 nconf.argv() // command line arguments
   .env() // enviroment variables
-  .file(`./config/database.${process.env.NODE_ENV}.json`); // config file
+  .file(`./config/database.${env}.json`); // config file
 
 module.exports = neo4j.driver(nconf.get('HOST'),
   neo4j.auth.basic(nconf.get('USERNAME'), nconf.get('PASSWORD'))
